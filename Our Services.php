c61e3a1c7570d326f671eff4c7e3368abe5f999c
Hellennjerik <hellen.karanja@strathmore.edu>
2024-07-07 22:35:08 -0500

    our services php

---
+++ Our Services.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet"href="Style.CSS">
+</head>
+<body>
+    <!--navigation bar-->
+    <div class="topnav">
+        <a class="active" href="#">Home</a>
+        <a href="Popular Destinations.php">Popular Destinations</a>
+        <a href="Our Services.php"> Our services</a>
+        <a href="Feedback.php">Feedback</a>
+        <a  href="Booking.php">Book</a>
+        <a href="about.php">About Us</a>
+        <a href="index.php">Home</a>
+        <a href="Packages.php">Packages</a>
+    </div>
+   
+<?php include_once("Templates/footer.php");?>
+
+     
+
+</body>
+</html>
