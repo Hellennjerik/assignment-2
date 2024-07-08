20c91e9439dd5a21eaca5567f3f8dc8693e1cade
Hellennjerik <hellen.karanja@strathmore.edu>
2024-07-07 23:28:32 -0500

    adding the footer link

--- Popular Destinations.php
+++ Popular Destinations.php
@@ -89,40 +89,9 @@
 
     </div>
 
+    <?php include_once("Templates/footer.php");?>
 
-    <!--Footer-->
- <section class="footer">
-    <div class="box-container">
-        <div class="box">
-            <h3>Quick links</h3>
-            <a href="Popular Destinations.php"><i class="fas fa-angle-right"></i> Popular Destinations</a>
-            <a href="Our Services.php"><i class="fas fa-angle-right"></i> Our Services</a>
-            <a href="Feedback.php"><i class="fas fa-angle-right"></i> Feedback</a>
-            <a href="Booking.php"><i class="fas fa-angle-right"></i> Book</a>
-            <a href="about.php"><i class="fas fa-angle-right"></i> About Us</a>
-            <a href="index.php"><i class="fas fa-angle-right"></i> Home</a>
-            <a href="Packages.php"><i class="fas fa-angle-right"></i> Packages</a>
-        </div>
-
-        <div class="box">
-            <h3>Contacts Info</h3>
-            <a href="tel:0712345678"><i class="fas fa-phone"></i> 0712345678</a>
-            <a href="tel:0787654321"><i class="fas fa-phone"></i> 0787654321</a>
-            <a href="mailto:mahabatravels@gmail.com"><i class="fas fa-envelope"></i> mahabatravels@gmail.com</a>
-        </div>
-
-        <div class="box">
-            <h3>Follow us</h3>
-            <ul>
-                <li><a href="https://www.instagram.com/"><i class="fab fa-instagram"></i></a></li>
-                <li><a href="https://twitter.com/"><i class="fab fa-twitter"></i></a></li>
-                <li><a href="https://web.whatsapp.com/"><i class="fab fa-whatsapp"></i></a></li>
-                <li><a href="https://www.facebook.com/"><i class="fab fa-facebook"></i></a></li>
-            </ul>
-        </div>
-    </div>
-</section>
- 
+    
 
 </body>
 </html>
