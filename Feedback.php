26f81e378f4ae9eb3c9386be697adf1de972c0dc
Hellennjerik <hellen.karanja@strathmore.edu>
2024-07-07 22:35:49 -0500

    feedback

---
+++ Feedback.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Feedback</title>
+    <link rel="stylesheet"href="Style.CSS">
+    <script src="https://kit.fontawesome.com/c8e4d183c2.js"crosorigin="anonymous"></script>
+</head>
+<body>
+    <section id="Feedback">
+        <div class="Feedback-heading">
+            <span>Reviews</span>
+            <h1>Travelers Feedback</h1>
+        </div>
+
+        <div class="Feedback-box-container">
+           <div class="Feedback-box">
+            <div class="box-top">
+                <div class="profile"></div>
+                <div class="profile-image">
+                    <img src="Images/icon1.png">
+                </div>
+                 <div class="username">
+                    <strong>John Nderitu</strong>
+                    <span>@Johnndetss254</span>
+
+                 </div>
+
+                <div class="reviews">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="far fa-star"></i>
+                </div>
+            </div>
+
+            <div class="Travelers feedback">
+                <p>This is my first time using Mahaba Travel<br>They have exceed my expectation and they deliver what they promise<br>I went to Dubai that was one of the popular destinations and it was more than what i expected.</p>
+
+            </div>
+
+           </div>
+
+           <div class="Feedback-box-container">
+            <div class="Feedback-box">
+             <div class="box-top">
+                 <div class="profile"></div>
+                 <div class="profile-image">
+                     <img src="Images/icon 2.jpeg">
+                 </div>
+                  <div class="username">
+                     <strong>Njeri Nduta</strong>
+                     <span>@NdutsRinje098</span>
+ 
+                  </div>
+ 
+                 <div class="reviews">
+                     <i class="fas fa-star"></i>
+                     <i class="fas fa-star"></i>
+                     <i class="fas fa-star"></i>
+                     <i class="fas fa-star"></i>
+                     <i class="fas fa-star"></i>
+                 </div>
+             </div>
+ 
+             <div class="Travelers feedback">
+                 <P>An amazing Time was heard<br> Definetly using this company for my next trip.</P>
+ 
+             </div>
+ 
+            </div>
+
+            <div class="Feedback-box-container">
+                <div class="Feedback-box">
+                 <div class="box-top">
+                     <div class="profile"></div>
+                     <div class="profile-image">
+                         <img src="Images/icon1.png">
+                     </div>
+                      <div class="username">
+                         <strong>Zack Omondi</strong>
+                         <span>@OmondiZack876</span>
+     
+                      </div>
+     
+                     <div class="reviews">
+                         <i class="fas fa-star"></i>
+                         <i class="fas fa-star"></i>
+                         <i class="fas fa-star"></i>
+                         <i class="fas fa-star"></i>
+                         <i class="far fa-star"></i>
+                     </div>
+                 </div>
+     
+                 <div class="Travelers feedback">
+                     <p>I was not sure about where to go for a family vacation and when I saw this website with the popular destinations,I decided to go to one<br>It was a good choice,choose them for all your travels.</p>
+     
+                 </div>
+     
+                </div>
+
+                <div class="Feedback-box-container">
+                    <div class="Feedback-box">
+                     <div class="box-top">
+                         <div class="profile"></div>
+                         <div class="profile-image">
+                             <img src="Images/icon 2.jpeg">
+                         </div>
+                          <div class="username">
+                             <strong>Jane Njoki</strong>
+                             <span>@Janenjoks244</span>
+         
+                          </div>
+         
+                         <div class="reviews">
+                             <i class="fas fa-star"></i>
+                             <i class="fas fa-star"></i>
+                             <i class="fas fa-star"></i>
+                             <i class="far fa-star"></i>
+                             <i class="far fa-star"></i>
+                         </div>
+                     </div>
+         
+                     <div class="Travelers feedback">
+                         <P>Amazing company although an improve on the services provided mostly the Long distance travels using buses</P>
+         
+                     </div>
+         
+                    </div>
+
+                    
+                        </div>
+
+        </div>
+    </section>
+
+
+    <!--Footer-->
+    <section class="footer">
+        <div class="box-container">
+            <div class="box">
+                <h3>Quick links</h3>
+                <a href="Popular Destinations.php"><i class="fas fa-angle-right"></i>Popular Destinations</a>
+                <a href="Our Services.php"><i class="fas fa-angle-right"></i>Our Services</a>
+                <a href="Feedback.php"><i class="fas fa-angle-right"></i>Feedback</a>
+                <a href="Booking.php"><i class="fas fa-angle-right"></i>Book</a>
+                <a href="about.php"><i class="fas fa-angle-right"></i>About Us</a>
+                <a href="index.php"><i class="fas fa-angle-right"></i>Home</a>
+                <a href="Packages.php"><i class="fas fa-angle-right"></i>Packages</a>
+
+            </div>
+
+            <div class="box">
+                <h3>Contacts Info</h3>
+                <a href="tel:0712345678"><i class="fas fa-phone"></i>0712345678</a>
+                <a href="tel:0787654321"><i class="fas fa-phone"></i>0787654321</a>
+                <a href="malito:mahabatravels@gmail.com"><i class="fas fa-email"></i>mahabatravels@gmail.com</a>
+            </div>
+
+            <div class="box">
+                <h3>Follow us</h3>
+                <li><a href="https://www.instagram.com/"><i class="fa-brands fa-instagram"></i></a></li>
+                <li><a href="https://twitter.com/"><i class="fa-brands fa-twitter"></i></a></li>
+                <li><a href="https://web.whatsapp.com/"><i class="fa-brands fa-whatsapp"></i></a></li>
+                <li><a href="https://ke.linkedin.com/"><i class="fa-brands fa-facebook"></i></a></li>
+              </ul>
+            </div>
+
+        </div>
+     </section>
+     
+       
+</body>
+</html>
