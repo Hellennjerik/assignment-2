463c2015287cec2c1fefa48e972527f300f6d4fc
Hellennjerik <hellen.karanja@strathmore.edu>
2024-07-07 22:29:40 -0500

    change to php

---
+++ about.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About us </title>
+    <link rel="stylesheet"href="Style.CSS">
+</head>
+<body>
+
+    <div class="about-msg">
+        <h1>About Us</h1>
+        <h3>Welcome to Mahaba Travels</h3>
+        <P>Mahaba which is our copmpany name means love,we have passion and love for travel and adventure hence our company name Mahaba Travels.<br>At Mahaba Travel,we believe it is not just about visiting a new palce it is about the memories we create and the unforgattable experiences that enrich your life.<br>Our mission is to create exceptional travel experiences and bring those dreams into reality.</P>
+        <h2>Our Story</h2>
+        <P>Mahaba was created by a team of friends who had a thirst for travel and they wanted people to experience the same.<br>What started as adream for friendship adventures has now turned to a dedicated platform that enables travel enthusiasts to discover new places and enjoy the world</P>
+        <h2>Why Choose Us</h2>
+        <p>Over the years we have have gained experiences in the travel industry that we take pride in.<br>We prioritize satisfaction,offering 24/7 support and attention every step of the way.<br>Our commitment to a nice travel ensures that your adventures enrich your life and uttermost satisfaction is received</p>
+        <h2>Join Us On Your Next Adventure</h2>
+        <P>Join us in your new adventure whether it is your first time abroad,business venture,family vaction,Mahaba Travels invites you to explore the world with us.<br>Start your Journey today and let us be your trusted companion in travel</P>
+    </div>
+   
+
+
+    <!--Footer-->
+    <section class="footer">
+        <div class="box-container">
+            <div class="box">
+            <a href="Popular Destinations.php"><i class="fas fa-angle-right"></i>Popular Destinations</a>
+                <a href="Our Services.php"><i class="fas fa-angle-right"></i>Our Services</a>
+                <a href="Feedback.php"><i class="fas fa-angle-right"></i>Feedback</a>
+                <a href="Booking.php"><i class="fas fa-angle-right"></i>Book</a>
+                <a href="about.php"><i class="fas fa-angle-right"></i>About Us</a>
+                <a href="index.php"><i class="fas fa-angle-right"></i>Home</a>
+                <a href="Packages.php"><i class="fas fa-angle-right"></i>Packages</a>
+            </div>
+
+            <div class="box">
+                <h3>Contacts Info</h3>
+                <a href="tel:0712345678"><i class="fas fa-phone"></i>0712345678</a>
+                <a href="tel:0787654321"><i class="fas fa-phone"></i>0787654321</a>
+                <a href="malito:mahabatravels@gmail.com"><i class="fas fa-email"></i>mahabatravels@gmail.com</a>
+            </div>
+
+            <div class="box">
+                <h3>Follow us</h3>
+                <li><a href="https://www.instagram.com/"><i class="fa-brands fa-instagram"></i></a></li>
+                <li><a href="https://twitter.com/"><i class="fa-brands fa-twitter"></i></a></li>
+                <li><a href="https://web.whatsapp.com/"><i class="fa-brands fa-whatsapp"></i></a></li>
+                <li><a href="https://ke.linkedin.com/"><i class="fa-brands fa-facebook"></i></a></li>
+              </ul>
+            </div>
+
+        </div>
+     </section>
+     
+</body>
+</html>
