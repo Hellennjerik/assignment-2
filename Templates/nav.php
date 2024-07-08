72a4f57eee43cbfaff97762a057721221be49d1f
Hellennjerik <hellen.karanja@strathmore.edu>
2024-07-07 23:16:37 -0500

    adding the navigation code

---
+++ Templates/nav.php
@@ -0,0 +1,12 @@
+ <!--navigation bar-->
+ <div class="topnav">
+        <a class="active" href="#">Home</a>
+        <a href="Popular Destinations.php">Popular Destinations</a>
+        <a href="Our Services.php"> Our services</a>
+        <a href="Feedback.php">Feedback</a>
+        <a href="about.php">About Us</a>
+        <a href="index.php">Home</a>
+        <a href="Packages.php">Packages</a>
+
+        <a href="Booking.php" class="Book-btn">Book Now</a>
+    </div>
