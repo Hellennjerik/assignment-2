32a4fae45e5366699d6bf0c1dd9a154a42ae999c
Hellennjerik <hellen.karanja@strathmore.edu>
2024-07-07 22:34:24 -0500

    popular destinations

---
+++ Popular Destinations.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Popular Destinations</title>
+    <link rel="stylesheet"href="Style.CSS">
+</head>
+<body>
+    <div class="container">
+        <h2 class="sub-title">Popular Destinations</h2>
+        <div class="Popular Destinations">
+            <div class="row">
+                <div class="column">
+            <div class="card">
+                <img src="Images/Image  1.jpeg"width= "70%">
+                <span>
+                    <h3>Burj Khalifa,Dubai</h3>
+                    <p>Burj khalifa offers a unique blend of breathtaking views ,luxury amenities and the nice architecture. </p>
+                    <P>It offers nice views of Dubai and the surrounding areas attracting tourists who want to see the city.</P>
+                </span>
+            </div>
+        </div>
+        </div>
+        <div class="row">
+            <div class="column">
+            <div class="card">
+                <img src="Images/image 2.jpg" width="80%">
+                <span>
+                    <h3>Paris,France</h3>
+                    <P>It has stanning landmarks like the eiffel tower which draw millions of tourists to admire those landmarks. </P>
+                    <P>Paris is also a centre of fashion and culture it hosts world-class art galaries and fashion events and is known as the city of love due to its romantic ambience.</P>
+                </span>
+            </div>
+        </div>
+        </div>
+        <div class="row">
+            <div class="column">
+            <div class="card">
+                <img src="Images/image 3.jpg"width="80%">
+                <span>
+                    <h3>Maasai Mara,Kenya</h3>
+                    <P>It has unique safari activities,amazing landscapes and exceptional cultural experiences.</P>
+                    <P>Activities such as bird watching,Ballon safaris  and the great migration are among of the activities that attract tourist here.</P>
+                </span>
+            </div>
+        </div>
+        </div>
+        <div class="row">
+            <div class="column">
+            <div class="card">
+                <img src="Images/image 4.jpeg"width="80%">
+                <span>
+                    <h3>Ait Ben Haddou,Morocco</h3>
+                    <P>The place has historical significance ,architectural beauty,stunning landscapes and cultutal richness that is a memorable stop for travelers exploring Morocco.</P>
+                    <P>It has also served as a backdrop for many known movies and it teaches people about the berber culture and history.</P>
+                </span>
+            </div>
+        </div>
+        </div>
+        <div class="row">
+            <div class="column">
+            <div class="card">
+                <img src="Images/image 5.jpeg"width="80%">
+                <span>
+                    <h3>Santorini,Greece</h3>
+                    <P>For travellers seeeking a memorable experience this is the place to be due to the stunning natural beauty of the place,cultural richness and romantic ambience.</P>
+                    <P>The culniray experience with its fresh saefood and locally produced wine and traditional cuisine.</P>
+                    
+                </span>
+            </div>
+        </div>
+        </div>
+        <div class="row">
+            <div class="column">
+            <div class="card">
+                <img src="Images/image 6.webp"width="80%">
+                <span>
+                    <h3>Taj Mahal,India</h3>
+                    <P>It is one of the world's most iconic landmarks with its historical significance and cultural richness  </P>
+                    <P>It is a symbol of love as it was the emperor's ddevotion to his wife and the photographic opportunities especcially during sunrise and the sunset ehen the light casts different hues on the marble surface.</P>
+                </span>
+            </div>
+        </div>
+        </div>
+
+        </div>
+
+    </div>
+
+
+    <!--Footer-->
+ <section class="footer">
+    <div class="box-container">
+        <div class="box">
+            <h3>Quick links</h3>
+            <a href="Popular Destinations.php"><i class="fas fa-angle-right"></i> Popular Destinations</a>
+            <a href="Our Services.php"><i class="fas fa-angle-right"></i> Our Services</a>
+            <a href="Feedback.php"><i class="fas fa-angle-right"></i> Feedback</a>
+            <a href="Booking.php"><i class="fas fa-angle-right"></i> Book</a>
+            <a href="about.php"><i class="fas fa-angle-right"></i> About Us</a>
+            <a href="index.php"><i class="fas fa-angle-right"></i> Home</a>
+            <a href="Packages.php"><i class="fas fa-angle-right"></i> Packages</a>
+        </div>
+
+        <div class="box">
+            <h3>Contacts Info</h3>
+            <a href="tel:0712345678"><i class="fas fa-phone"></i> 0712345678</a>
+            <a href="tel:0787654321"><i class="fas fa-phone"></i> 0787654321</a>
+            <a href="mailto:mahabatravels@gmail.com"><i class="fas fa-envelope"></i> mahabatravels@gmail.com</a>
+        </div>
+
+        <div class="box">
+            <h3>Follow us</h3>
+            <ul>
+                <li><a href="https://www.instagram.com/"><i class="fab fa-instagram"></i></a></li>
+                <li><a href="https://twitter.com/"><i class="fab fa-twitter"></i></a></li>
+                <li><a href="https://web.whatsapp.com/"><i class="fab fa-whatsapp"></i></a></li>
+                <li><a href="https://www.facebook.com/"><i class="fab fa-facebook"></i></a></li>
+            </ul>
+        </div>
+    </div>
+</section>
+ 
+
+</body>
+</html>
