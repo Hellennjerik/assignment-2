c3b77cf334b0626c59e506a8e9dd69475fe20588
Hellennjerik <hellen.karanja@strathmore.edu>
2024-07-08 02:44:00 -0500

    adding transport link

--- Templates/nav.php
+++ Templates/nav.php
@@ -2,7 +2,7 @@
  <div class="topnav">
         <a class="active" href="#">Home</a>
         <a href="Popular Destinations.php">Popular Destinations</a>
-        <a href="Our Services.php"> Our services</a>
+        <a href="Transport.php"> Our services</a>
         <a href="Feedback.php">Feedback</a>
         <a href="about.php">About Us</a>
         <a href="index.php">Home</a>
