15d03bb3e360b484786872b9661e5a48698ec93f
Hellennjerik <hellen.karanja@strathmore.edu>
2024-07-08 02:42:44 -0500

    new name for our services

---
+++ Transport.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet"href="Style.CSS">
+</head>
+<body>
+<?php include_once("Templates/nav.php");?>
+
+<div class="container">
+        <h2 class="sub-title">Transport options</h2>
+        <div class="Transport">
+            <div class="row">
+                <div class="column">
+            <div class="card">
+                <img src="Images/free-photo-of-a-car-in-a-field-at-sunset.jpeg"width= "70%">
+                <span>
+                    <h3>Safari car</h3>
+                    <p>We offer safari cars that are used in and outside the country<br>This is a good option for those who love roadtrips<br>we offer a driver and a tourgide for this safari car for the travelers who need one </p>
+                </span>
+            </div>
+        </div>
+        </div>
+        <div class="row">
+            <div class="column">
+            <div class="card">
+                <img src="Images/pexels-photo-3275463.jpeg"width="80%">
+                <span>
+                    <h3>Personal car</h3>
+                    <p>The perfect solution for travelers who want to explore by themselves.<br>A self-drive to your dream vacation as you know the routes and enjoy the views around.</p>
+                </span>
+            </div>
+        </div>
+        </div>
+        <div class="row">
+            <div class="column">
+            <div class="card">
+                <img src="Images/pexels-photo-164589.webp"width="80%">
+                <span>
+                    <h3>Airplane</h3>
+                    <P>For trips outside the continent and also within the country and nearby countries.A convinient and budget friendly airline is chosen for you .</P>
+                </span>
+            </div>
+        </div>
+        </div>
+        <div class="row">
+            <div class="column">
+            <div class="card">
+                <img src="Images/istockphoto-585616168-612x612.jpg"width="80%">
+                <span>
+                    <h3>Tour Bus</h3>
+                    <P>It is seen a s an old-fashioned method of travel but the most convinient for large groups like office trips or big family trips.<br>There are different from the luxury ones to the normal tour buses all to carter to your needs and preferences as Traveler. </P>
+                </span>
+            </div>
+        </div>
+        </div>
+
+        </div>
+
+    </div>
+
+
+     
+
+</body>
+</html>
