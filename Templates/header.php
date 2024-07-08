58390f0c285b13033543d0288d468a873596516f
Hellennjerik <hellen.karanja@strathmore.edu>
2024-07-07 22:30:28 -0500

    header

---
+++ Templates/header.php
@@ -0,0 +1,20 @@
+<div class="header">
+        <nav>
+            
+            <ul class="nav-links">
+                <li><a href="Popular Destinations.php">Popular Destinations</a></li>
+                <li><a href="Our Services.php">Our Services</a></li>
+                <li><a href="Feedback.php">Feedback</a></li>
+                <li><a href="Booking.php">Book</a></li>
+                <li><a href="about.php">About Us</a></li>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="Packages.php">Packages</a></li>
+            </ul>
+            <a href="Booking.php" class="Book-btn">Book Now</a>
+        </nav>
+        <div class="container">
+            <h1>MAHABA TRAVELS</h1>
+            <h3>Find Your Dream Destination</h3>
+
+        </div>
+     </div>
